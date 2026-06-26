265ce7b24729249e6470a10a62fa1679dc518916
WildanBaihaqii <haaaqyyyy@example.com>
2026-06-26 10:58:59 +0700

    [Tahap 3] - Membuat Abstract

--- class/Mahasiswa.php
+++ class/Mahasiswa.php
@@ -1,14 +1,12 @@
 <?php
 
 abstract class Mahasiswa {
-    // Properti Terenkapsulasi (Protected) - Sesuai ketentuan Tahap 3
     protected int $id_mahasiswa;
     protected string $nama_mahasiswa;
     protected string $nim;
     protected int $semester;
-    protected float $tarifUktNominal; // Menggunakan float untuk tipe DECIMAL
+    protected float $tarifUktNominal;
 
-    // Constructor untuk inisialisasi properti global
     public function __construct(int $id_mahasiswa, string $nama_mahasiswa, string $nim, int $semester, float $tarifUktNominal) {
         $this->id_mahasiswa = $id_mahasiswa;
         $this->nama_mahasiswa = $nama_mahasiswa;
@@ -17,14 +15,26 @@ abstract class Mahasiswa {
         $this->tarifUktNominal = $tarifUktNominal;
     }
 
-    // --- ABSTRACT METHODS (Wajib tanpa bodi/isi) ---
+    public function getIdMahasiswa(): int {
+        return $this->id_mahasiswa;
+    }
+
+    public function getNamaMahasiswa(): string {
+        return $this->nama_mahasiswa;
+    }
+
+    public function getNim(): string {
+        return $this->nim;
+    }
+
+    public function getSemester(): int {
+        return $this->semester;
+    }
+
+    public function getTarifUktNominal(): float {
+        return $this->tarifUktNominal;
+    }
+
     abstract public function hitungTagihanSemester(): void;
     abstract public function tampilkanSpesifikasiAkademik(): void;
-
-    // --- GETTER & SETTER (Untuk akses terkontrol) ---
-    public function getIdMahasiswa(): int { return $this->id_mahasiswa; }
-    public function getNamaMahasiswa(): string { return $this->nama_mahasiswa; }
-    public function getNim(): string { return $this->nim; }
-    public function getSemester(): int { return $this->semester; }
-    public function getTarifUktNominal(): float { return $this->tarifUktNominal; }
 }
